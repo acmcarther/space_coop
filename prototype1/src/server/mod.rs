7a6681aec95e17fa3e5deaacfb5334b395bfe01f
Alex McArther <acmcarther@gmail.com>
2015-08-01 16:26:59 -0700

    Cull dead users

--- prototype1/src/server/mod.rs
+++ prototype1/src/server/mod.rs
@@ -3,6 +3,7 @@ pub use self::server::{start};
 mod server {
   use std::thread;
   use std::collections::HashMap;
+  use std::net::SocketAddr;
 
   use app_net::ServerNet;
   use params;
@@ -22,15 +23,34 @@ mod server {
     println!("Hello server!");
     let mut server_state = ServerState::new();
     let mut connected_users = HashMap::new();
+    let last_cull = SteadyTime::now();
 
     loop {
       thread::sleep_ms(20);
+      let now = SteadyTime::now();
+
+      if (now - last_cull).num_seconds() > 2 {
+        let dead_users =
+          connected_users
+            .iter()
+            .filter(|&(_, last_contact): &(_, &SteadyTime)| (now - last_contact.clone()).num_seconds() > 5)
+            .map(|(&user_addr, _): (&SocketAddr, _)| user_addr.clone())
+            .collect::<Vec<SocketAddr>>();
+
+        dead_users
+          .iter()
+          .foreach(|dead_user_addr| {
+            connected_users.remove(&dead_user_addr);
+          });
+      }
 
       let events = app_network.get_events();
       events
         .into_iter()
         .foreach(|(source, event)| {
           if connected_users.contains_key(&source) {
+            connected_users.insert(source.clone(), SteadyTime::now());
+
             match event {
               ClientEvent::KeepAlive => {
                 app_network.send_event(source.clone(), ServerEvent::KeepAlive);
