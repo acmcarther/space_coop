0f0ab07aaec445896cd31a818bd6f04413bea2fe
Alex McArther <acmcarther@gmail.com>
2015-08-01 16:10:40 -0700

    Server infrastructure for client server connection

--- prototype1/src/client/mod.rs
+++ prototype1/src/client/mod.rs
@@ -13,6 +13,7 @@ mod client {
   use std::thread;
 
   use itertools::Itertools;
+  use time::SteadyTime;
 
   pub fn start() {
     let state = ClientState::new();
@@ -20,8 +21,16 @@ mod client {
     let app_network = ClientNet::new(client_params.addr, client_params.server_addr);
     println!("Hello client!");
     app_network.send_event(ClientEvent::Connect);
+    let mut last_sent = SteadyTime::now();
     loop {
       thread::sleep_ms(20);
+
+      let now = SteadyTime::now();
+      if (now - last_sent).num_seconds() > 1 {
+        last_sent = now;
+        app_network.send_event(ClientEvent::KeepAlive);
+      }
+
       let events = app_network.get_events();
       events
         .into_iter()
--- prototype1/src/server/mod.rs
+++ prototype1/src/server/mod.rs
@@ -2,20 +2,55 @@ pub use self::server::{start};
 
 mod server {
   use std::thread;
+  use std::collections::HashMap;
 
   use app_net::ServerNet;
   use params;
   use state::ServerState;
+
+  use events::{
+    ClientEvent,
+    ServerEvent
+  };
+
+  use itertools::Itertools;
+  use time::SteadyTime;
 
   pub fn start() {
     let server_params = params::query_server_params();
     let app_network = ServerNet::new(server_params.addr);
     println!("Hello server!");
     let mut server_state = ServerState::new();
+    let mut connected_users = HashMap::new();
 
     loop {
       thread::sleep_ms(20);
+
       let events = app_network.get_events();
+      events
+        .into_iter()
+        .foreach(|(source, event)| {
+          if connected_users.contains_key(&source) {
+            match event {
+              ClientEvent::KeepAlive => {app_network.send_event(source, ServerEvent::KeepAlive);},
+              ClientEvent::Connect => {app_network.send_event(source, ServerEvent::Connected);},
+              ClientEvent::Disconnect => {
+                connected_users.remove(&source);
+                app_network.send_event(source, ServerEvent::NotConnected);
+              },
+              ClientEvent::Chat => println!("{:?} chatted", source),
+              ClientEvent::TryMove => println!("{:?} tried to move", source),
+            }
+          } else {
+            match event {
+              ClientEvent::Connect => {
+                connected_users.insert(source.clone(), SteadyTime::now());
+                app_network.send_event(source, ServerEvent::Connected);
+              },
+              _ => {app_network.send_event(source, ServerEvent::NotConnected);}
+            }
+          }
+        })
     }
   }
 }
